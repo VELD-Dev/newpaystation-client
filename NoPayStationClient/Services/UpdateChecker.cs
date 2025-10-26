4773df23eb4904ffc5d3805cd9fe7d0704a6422a
VELD-Dev <evan.robin07@gmail.com>
2025-10-27 00:55:36 +0100

    Added base for UpdateChecker & SelfUpdateChecker

---
+++ NoPayStationClient/Services/UpdateChecker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewPayStation.Client.Services;
+
+internal class UpdateChecker
+{
+    // Todo: implement self-update check using SHA256 hashes comparison (?)
+    // Todo: implement TSV files update check using SHA256 hashes comparison
+    
+    // I'm just tired rn so i'm leaving this for later :)
+}
